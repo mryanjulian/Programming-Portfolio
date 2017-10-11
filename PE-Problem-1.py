d97fed00adea618489e126bcc688dfc412d43084
Ryan Julian <mrjulian@wisc.edu>
2017-10-10 21:54:29 -0500

    Update PE-Problem-1.py

--- PE-Problem-1.py
+++ PE-Problem-1.py
@@ -1,1 +1,10 @@
+#This program computes the sum of all the multiples of 3 or 5 below 1000
 
+s = 0
+for i in range(1,334):
+	s += 3*i
+for i in range(1,200):
+	s += 5*i
+for i in range(1,67):
+	s -= 15*i
+print s
